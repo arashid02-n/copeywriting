210edcdc7914fa8f7c7f1ee0d4281f3493397616
Rashid <rashidnzri@gmail.com>
2025-10-11 18:07:22 +0330

    Update login.py

--- pages/login.py
+++ pages/login.py
@@ -40,7 +40,8 @@ authenticator = stauth.Authenticate(
 # -------------------------------
 # Normal login form
 # -------------------------------
-name, authentication_status, username = authenticator.login("Login", "main")
+name, authentication_status, username = authenticator.login("Login")  # یا location="main" در نسخه جدید
+
 
 if authentication_status:
     st.session_state["user"] = {"name": name, "username": username}
